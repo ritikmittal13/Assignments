048d7d194998f2ca95f2998df2335ee7ff79f79b
Ritik Mittal <ritikmittal13@gmail.com>
2018-07-12 19:36:35 +0530

    Routes

---
+++ todo_express/routes/todo.js
@@ -0,0 +1,26 @@
+const route=require('express').Router()
+
+let todo=[
+    {task: "Go to Market", done:"Y"},
+    {task: "Go to Laundry", done:"N"}
+]
+route.get('/',(req,res)=>res.send(todo))
+route.get('/:id',(req,res)=>res.send(todo[req.params.id]))
+route.post('/',(req,res)=>{
+    todo.push({
+        task: req.body.task,
+        done: req.body.done
+    })
+    res.send(todo)
+})
+route.patch('/:id',function(req,res){
+    todo[req.params.id].task=req.body.task
+    todo[req.params.id].done=req.body.done
+    res.send(todo)
+})
+route.delete('/:id',function(req,res){
+    todo.splice(req.params.id,1)
+    res.send(todo)
+})
+
+module.exports=route
--- todo_express/server.js
+++ todo_express/server.js
@@ -1,28 +1,9 @@
 const express=require('express')
 const server=express()
+const todoRoute=require('./routes/todo')
 
-let todo=[
-    {task: "Go to Market", done:"Y"},
-    {task: "Go to Laundry", done:"N"}
-]
 server.use(express.json())
 server.use(express.urlencoded({extended: true}))
-server.get('/todo',(req,res)=>res.send(todo))
-server.get('/todo/:id',(req,res)=>res.send(todo[req.params.id]))
-server.post('/todo',(req,res)=>{
-    todo.push({
-        task: req.body.task,
-        done: req.body.done
-    })
-    res.send(todo)
-})
-server.patch('/todo/:id',function(req,res){
-    todo[req.params.id].task=req.body.task
-    todo[req.params.id].done=req.body.done
-    res.send(todo)
-})
-server.delete('/todo/:id',function(req,res){
-    todo.splice(req.params.id,1)
-    res.send(todo)
-})
+server.use('/todo',todoRoute)
+server.use('/public',express.static(__dirname+"/public/todolist.html"))
 server.listen(1980)
